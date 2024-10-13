c7118f4b7ebca97c32724ac5cef4793b108e34c3
Oseyijotham <jothamoboh16@gmail.com>
2024-10-13 21:16:33 +0100

    Modal Mobile Update

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import css from './MainPage.module.css';
 import { useState } from 'react';
+import backArrow from './backArrow.png';
 
 export const MainPage = () => {
 const [bloodType, setBloodType] = useState('');
 const [height, setHeight] = useState('');
 const [age, setAge] = useState('');
 const [cWeight, setCWeight] = useState('');
-const [dWeight, setDWeight] = useState('');
+  const [dWeight, setDWeight] = useState('');
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [result, setResult] = useState();
   
   const handleBloodTypeChange = event => {
     setBloodType(event.target.value);
@@ -26,15 +29,52 @@ const [dWeight, setDWeight] = useState('');
   };
 
 
+  const handleModalClose = event => {
+    setModalOpen(false);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const dailyCalorieIntake =
+      Math.ceil (10 * cWeight +
+      6.25 * height -
+      5 * age -
+      161 -
+      10 * (cWeight - dWeight));
+    setResult(dailyCalorieIntake);
+   setModalOpen(true);
+  }
+
   return (
     <div className="max-w-[1400px] mx-auto">
-      <div className={css.pageContainer}>
-        <div className={css.overlayWrapper}>
+      <div className={css.overlayWrapper}>
+        {isModalOpen === true && (
           <div className={css.overlay}>
-            <div className={css.formModal}></div>
+            <button className={css.overlayClose} onClick={handleModalClose}>
+              <img src={backArrow} width="12px" height="7px" />
+            </button>
+            <div className={css.modal}>
+              <div className={css.modalTitle}>
+                Your recommended daily calorie intake is
+              </div>
+              <div className={css.modalValueWrapper}>
+                <span className={css.modalValue}>{result}</span>
+                <span className={css.modalValueUnit}>ккал</span>
+              </div>
+              <div className={css.modalHeading}>Foods you should not eat</div>
+              <ul className={css.modalList}>
+                <li>Flour products</li>
+                <li>Milk</li>
+                <li>Red meat</li>
+                <li>Smoked meats</li>
+              </ul>
+              <button className={css.button}>Start losing weight</button>
+            </div>
           </div>
+        )}
+        <div className={css.pageContainer}>
           <div className={css.formWrapper}>
-            <form className={css.form}>
+            <form className={css.form} onSubmit={handleSubmit}>
               <span className={css.formTitle}>
                 <span className={css.formTitleSection}>
                   Calculate your daily calorie
@@ -45,13 +85,15 @@ const [dWeight, setDWeight] = useState('');
                 <div className={css.formSection}>
                   <label className={css.label}>
                     {height === '' && (
-                      <span className={css.labelText}>Height *</span>
+                      <span className={css.labelText}>
+                        Height<span className={css.labelTextSmall}>(cm)</span> *
+                      </span>
                     )}
                     <input
                       type="number"
                       name="height"
                       className={css.input}
-                      title="Enter your height"
+                      title="Enter your height in Centimeters"
                       autoComplete="off"
                       required
                       onChange={handleHeightChange}
@@ -71,13 +113,16 @@ const [dWeight, setDWeight] = useState('');
                   </label>
                   <label className={css.label}>
                     {cWeight === '' && (
-                      <span className={css.labelText}>Current weight *</span>
+                      <span className={css.labelText}>
+                        Current weight
+                        <span className={css.labelTextSmall}>(kg)</span> *
+                      </span>
                     )}
                     <input
                       type="number"
-                      name="height"
+                      name="currentWeight"
                       className={css.input}
-                      title="Enter your Current weight"
+                      title="Enter your Current weight in Kilograms"
                       autoComplete="off"
                       required
                       onChange={handleCurrentWeightChange}
@@ -87,13 +132,16 @@ const [dWeight, setDWeight] = useState('');
                 <div className={css.formSection}>
                   <label className={css.label}>
                     {dWeight === '' && (
-                      <span className={css.labelText}>Desired weight *</span>
+                      <span className={css.labelText}>
+                        Desired weight
+                        <span className={css.labelTextSmall}>(kg)</span> *
+                      </span>
                     )}
                     <input
                       type="number"
-                      name="height"
+                      name="desiredWeight"
                       className={css.input}
-                      title="Enter your Desired weight"
+                      title="Enter your Desired weight in Kilograms"
                       autoComplete="off"
                       required
                       onChange={handleDesiredWeight}
@@ -106,7 +154,7 @@ const [dWeight, setDWeight] = useState('');
                       <div className={css.radioOptionsWrapper}>
                         <input
                           type="radio"
-                          name="blood-type"
+                          name="blood-type-1"
                           value="1"
                           className={`${css.radioArea} ${css.radioInput}`}
                           id="1"
@@ -121,7 +169,7 @@ const [dWeight, setDWeight] = useState('');
                       <div className={css.radioOptionsWrapper}>
                         <input
                           type="radio"
-                          name="blood-type"
+                          name="blood-type-2"
                           value="2"
                           className={`${css.radioArea} ${css.radioInput}`}
                           id="2"
@@ -136,7 +184,7 @@ const [dWeight, setDWeight] = useState('');
                       <div className={css.radioOptionsWrapper}>
                         <input
                           type="radio"
-                          name="blood-type"
+                          name="blood-type-3"
                           value="3"
                           className={`${css.radioArea} ${css.radioInput}`}
                           id="3"
@@ -151,7 +199,7 @@ const [dWeight, setDWeight] = useState('');
                       <div className={css.radioOptionsWrapper}>
                         <input
                           type="radio"
-                          name="blood-type"
+                          name="blood-type-4"
                           value="4"
                           className={`${css.radioArea} ${css.radioInput}`}
                           id="4"
