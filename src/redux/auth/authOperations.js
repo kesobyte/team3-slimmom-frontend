2370288f6a841c3629dcb856abe52bbd050bf637
kesobyte <kimangeloalvarez@gmail.com>
2024-10-13 21:54:00 +0800

    Login & Verification Validation Update

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -79,11 +79,11 @@ export const login = createAsyncThunk(
       const response = await axios.post('/auth/login', credentials);
       const { user, accessToken, refreshToken } = response.data;
 
-      // // Check if the user is verified
+      // Check if the user is verified
       // if (!user.verified) {
       //   toast.error('Please verify your email before logging in.');
       //   return thunkAPI.rejectWithValue({
-      //     status: 403,
+      //     status: 401,
       //     message: 'Email not verified',
       //   });
       // }
@@ -95,8 +95,19 @@ export const login = createAsyncThunk(
       const status = error.response?.status;
       const message = error.response?.data?.message || error.message;
 
-      if (status === 401) {
+      if (status === 401 && message === 'Please verify your email') {
+        toast.error('Please verify your email.');
+      }
+
+      if (
+        (status === 401 && message === 'Invalid user') ||
+        message === 'Invalid credentials'
+      ) {
         toast.error('Invalid credentials, please try again.');
+      }
+
+      if (status === 400) {
+        toast.warning('Please enter valid email address');
       }
 
       return thunkAPI.rejectWithValue({ status, message });
@@ -174,9 +185,11 @@ export const resendVerifyEmail = createAsyncThunk(
       const message = error.response?.data?.message || error.message;
 
       if (status === 404) {
-        toast.error('Email not registered.');
-      } else if (status === 400) {
+        toast.error('Email is not registered.');
+      } else if (status === 400 && message === 'Email is already verified') {
         toast.info('Email was already verified.');
+      } else if (status === 400) {
+        toast.warning('Please enter valid email address');
       }
 
       return thunkAPI.rejectWithValue({ status, message });
