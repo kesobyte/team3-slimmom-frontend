aaed49709199b22d0896648e40aa12381324f341
Oseyijotham <jothamoboh16@gmail.com>
2024-10-13 19:00:03 +0100

    Update

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -28,144 +28,149 @@ const [dWeight, setDWeight] = useState('');
 
   return (
     <div className="max-w-[1400px] mx-auto">
-      <div className="xl:pt-[160px] md:pt-[100px] pt-[32px] xl:px-[20px] md:px-[32px] px-[20px]">
-        <div className={css.formWrapper}>
-          <form className={css.form}>
-            <span className={css.formTitle}>
-              <span className={css.formTitleSection}>
-                Calculate your daily calorie
+      <div className={css.pageContainer}>
+        <div className={css.overlayWrapper}>
+          <div className={css.overlay}>
+            <div className={css.formModal}></div>
+          </div>
+          <div className={css.formWrapper}>
+            <form className={css.form}>
+              <span className={css.formTitle}>
+                <span className={css.formTitleSection}>
+                  Calculate your daily calorie
+                </span>
+                <span className={css.formTitleSection}>intake right now</span>
               </span>
-              <span className={css.formTitleSection}>intake right now</span>
-            </span>
-            <div className={css.formInputs}>
-              <div className={css.formSection}>
-                <label className={css.label}>
-                  {height === '' && (
-                    <span className={css.labelText}>Height *</span>
-                  )}
-                  <input
-                    type="number"
-                    name="height"
-                    className={css.input}
-                    title="Enter your height"
-                    autoComplete="off"
-                    required
-                    onChange={handleHeightChange}
-                  />
-                </label>
-                <label className={css.label}>
-                  {age === '' && <span className={css.labelText}>Age *</span>}
-                  <input
-                    type="number"
-                    name="age"
-                    className={css.input}
-                    title="Enter your age"
-                    autoComplete="off"
-                    required
-                    onChange={handleAgeChange}
-                  />
-                </label>
-                <label className={css.label}>
-                  {cWeight === '' && (
-                    <span className={css.labelText}>Current weight *</span>
-                  )}
-                  <input
-                    type="number"
-                    name="height"
-                    className={css.input}
-                    title="Enter your Current weight"
-                    autoComplete="off"
-                    required
-                    onChange={handleCurrentWeightChange}
-                  />
-                </label>
-              </div>
-              <div className={css.formSection}>
-                <label className={css.label}>
-                  {dWeight === '' && (
-                    <span className={css.labelText}>Desired weight *</span>
-                  )}
-                  <input
-                    type="number"
-                    name="height"
-                    className={css.input}
-                    title="Enter your Desired weight"
-                    autoComplete="off"
-                    required
-                    onChange={handleDesiredWeight}
-                  />
-                </label>
+              <div className={css.formInputs}>
+                <div className={css.formSection}>
+                  <label className={css.label}>
+                    {height === '' && (
+                      <span className={css.labelText}>Height *</span>
+                    )}
+                    <input
+                      type="number"
+                      name="height"
+                      className={css.input}
+                      title="Enter your height"
+                      autoComplete="off"
+                      required
+                      onChange={handleHeightChange}
+                    />
+                  </label>
+                  <label className={css.label}>
+                    {age === '' && <span className={css.labelText}>Age *</span>}
+                    <input
+                      type="number"
+                      name="age"
+                      className={css.input}
+                      title="Enter your age"
+                      autoComplete="off"
+                      required
+                      onChange={handleAgeChange}
+                    />
+                  </label>
+                  <label className={css.label}>
+                    {cWeight === '' && (
+                      <span className={css.labelText}>Current weight *</span>
+                    )}
+                    <input
+                      type="number"
+                      name="height"
+                      className={css.input}
+                      title="Enter your Current weight"
+                      autoComplete="off"
+                      required
+                      onChange={handleCurrentWeightChange}
+                    />
+                  </label>
+                </div>
+                <div className={css.formSection}>
+                  <label className={css.label}>
+                    {dWeight === '' && (
+                      <span className={css.labelText}>Desired weight *</span>
+                    )}
+                    <input
+                      type="number"
+                      name="height"
+                      className={css.input}
+                      title="Enter your Desired weight"
+                      autoComplete="off"
+                      required
+                      onChange={handleDesiredWeight}
+                    />
+                  </label>
 
-                <div className={css.radioWrapper}>
-                  <div className={css.radioTitleLabel}>Blood type *</div>
-                  <div className={css.radioOptions}>
-                    <div className={css.radioOptionsWrapper}>
-                      <input
-                        type="radio"
-                        name="blood-type"
-                        value="1"
-                        className={`${css.radioArea} ${css.radioInput}`}
-                        id="1"
-                      />
-                      <label className={css.radioLabelWrapper} for="1">
-                        <span className={css.radioCircle}>
-                          <span className={css.radioSelector}></span>
-                        </span>
-                        <span className={css.radioLabelText}>1</span>
-                      </label>
-                    </div>
-                    <div className={css.radioOptionsWrapper}>
-                      <input
-                        type="radio"
-                        name="blood-type"
-                        value="2"
-                        className={`${css.radioArea} ${css.radioInput}`}
-                        id="2"
-                      />
-                      <label className={css.radioLabelWrapper} for="2">
-                        <span className={css.radioCircle}>
-                          <span className={css.radioSelector}></span>
-                        </span>
-                        <span className={css.radioLabelText}>2</span>
-                      </label>
-                    </div>
-                    <div className={css.radioOptionsWrapper}>
-                      <input
-                        type="radio"
-                        name="blood-type"
-                        value="3"
-                        className={`${css.radioArea} ${css.radioInput}`}
-                        id="3"
-                      />
-                      <label className={css.radioLabelWrapper} for="3">
-                        <span className={css.radioCircle}>
-                          <span className={css.radioSelector}></span>
-                        </span>
-                        <span className={css.radioLabelText}>3</span>
-                      </label>
-                    </div>
-                    <div className={css.radioOptionsWrapper}>
-                      <input
-                        type="radio"
-                        name="blood-type"
-                        value="4"
-                        className={`${css.radioArea} ${css.radioInput}`}
-                        id="4"
-                      />
-                      <label className={css.radioLabelWrapper} for="4">
-                        <span className={css.radioCircle}>
-                          <span className={css.radioSelector}></span>
-                        </span>
-                        <span className={css.radioLabelText}>4</span>
-                      </label>
+                  <div className={css.radioWrapper}>
+                    <div className={css.radioTitleLabel}>Blood type *</div>
+                    <div className={css.radioOptions}>
+                      <div className={css.radioOptionsWrapper}>
+                        <input
+                          type="radio"
+                          name="blood-type"
+                          value="1"
+                          className={`${css.radioArea} ${css.radioInput}`}
+                          id="1"
+                        />
+                        <label className={css.radioLabelWrapper} for="1">
+                          <span className={css.radioCircle}>
+                            <span className={css.radioSelector}></span>
+                          </span>
+                          <span className={css.radioLabelText}>1</span>
+                        </label>
+                      </div>
+                      <div className={css.radioOptionsWrapper}>
+                        <input
+                          type="radio"
+                          name="blood-type"
+                          value="2"
+                          className={`${css.radioArea} ${css.radioInput}`}
+                          id="2"
+                        />
+                        <label className={css.radioLabelWrapper} for="2">
+                          <span className={css.radioCircle}>
+                            <span className={css.radioSelector}></span>
+                          </span>
+                          <span className={css.radioLabelText}>2</span>
+                        </label>
+                      </div>
+                      <div className={css.radioOptionsWrapper}>
+                        <input
+                          type="radio"
+                          name="blood-type"
+                          value="3"
+                          className={`${css.radioArea} ${css.radioInput}`}
+                          id="3"
+                        />
+                        <label className={css.radioLabelWrapper} for="3">
+                          <span className={css.radioCircle}>
+                            <span className={css.radioSelector}></span>
+                          </span>
+                          <span className={css.radioLabelText}>3</span>
+                        </label>
+                      </div>
+                      <div className={css.radioOptionsWrapper}>
+                        <input
+                          type="radio"
+                          name="blood-type"
+                          value="4"
+                          className={`${css.radioArea} ${css.radioInput}`}
+                          id="4"
+                        />
+                        <label className={css.radioLabelWrapper} for="4">
+                          <span className={css.radioCircle}>
+                            <span className={css.radioSelector}></span>
+                          </span>
+                          <span className={css.radioLabelText}>4</span>
+                        </label>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <button className={css.button}>Start losing weight</button>
-          </form>
+              <button className={css.button}>Start losing weight</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
