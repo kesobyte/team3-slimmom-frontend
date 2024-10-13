3588e63623c363cabf31a1bf75a5d04d10887b03
katelim215 <katelimosnero215@gmail.com>
2024-10-13 14:21:02 +0800

    right side bar

---
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -0,0 +1,59 @@
+import { useSelector } from "react-redux";
+import { Wrapper, SummaryWrap, FoodWrap, Title, Item, Text, RedText } from './RightSideBar.styled';
+
+export const RightSideBar = () => {
+
+    const date = useSelector((state) => state.products.date);
+    const dailyRate = useSelector((state) => state.auth.userInfo.dailyRate);
+    const notAllowedProducts = useSelector((state) => state.auth.userInfo.notAllowedProducts);
+    const productsList = useSelector((state) => state.products.productsList);
+    const totalCalories = productsList.map(product => product.productCalories)
+        .reduce((prev, product) => { return Number.parseInt(prev) + Number.parseInt(product) }, 0);
+    const leftCalories = dailyRate - totalCalories;
+    const nOfNorm = (totalCalories / dailyRate) * 100;
+
+    return (
+        <Wrapper>
+            <SummaryWrap>
+                <Title>Summary for {date}</Title>
+                <ul>
+                    <Item>
+                        <Text>Left</Text>
+                        {leftCalories < 0 ?
+                            <RedText>{leftCalories} kcal</RedText> :
+                            <Text>{leftCalories ? leftCalories : '000'} kcal</Text> 
+                        }
+                    </Item>
+                    <Item>
+                        <Text>Consumed</Text>
+                        <Text>{totalCalories ? totalCalories : '000'} kcal</Text>
+                    </Item>
+                    <Item>
+                        <Text>Daily rate</Text>
+                        <Text>{dailyRate ? dailyRate : '000'} kcal</Text>
+                    </Item>
+                    <Item>
+                        <Text>n% of normal</Text>
+                        {nOfNorm > 100 ?
+                            <RedText>{nOfNorm ? Math.round(nOfNorm) : '0'} %</RedText> :
+                            <Text>{nOfNorm ? Math.round(nOfNorm) : '0'} %</Text> 
+                        }
+                    </Item>
+                </ul>
+            </SummaryWrap>
+            <FoodWrap>
+                <Title>Food not recommended</Title>
+                {notAllowedProducts ? 
+                    <ul>
+                        {notAllowedProducts.map((prod, index) => (
+                            <Text key={index}>
+                                {index + 1}. {prod}
+                            </Text>
+                        ))}
+                    </ul> :
+                    <Text>Your diet will be displayed here</Text>
+                }
+            </FoodWrap>
+        </Wrapper>
+    )
+};
---
+++ src/components/RightSideBar/RightSideBar.styled.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+// import from '';
+
+export const Wrapper = styled.div`
+  background-color: 'transparent';
+  padding: 40px 20px 84px 20px;
+
+  @media (min-width: 768px) and (max-width: 1023px) {
+    padding: 80px 0px 80px 32px;
+    display: flex;
+    // background-image: url();
+    background-repeat: no-repeat;
+    background-position: bottom 0px left 385px;
+    background-size: auto;
+  }
+  @media (min-width: 1024px) and (max-width: 1120px) {
+    background-color: transparent;
+    padding: 0px;
+    margin-right: 55px;
+  }
+  @media (min-width: 1121px) and (max-width: 1280px) {
+    margin-right: 105px;
+    padding: 0px;
+  }
+  @media (min-width: 1280px) {
+    margin-right: 105px;
+    padding: 0px;
+  }
+
+`;
+
+export const SummaryWrap = styled.div`
+  width: 280px;
+  margin: 0 auto;
+  margin-bottom: 40px;
+  @media (min-width: 768px) and (max-width: 1023px) {
+    width: 288px;
+    margin: 0;
+    margin-right: 80px;
+  }
+  @media (min-width: 1024px) {
+    margin-bottom: 60px;
+  }
+`;
+
+export const FoodWrap = styled.div`
+  width: 280px;
+  margin: 0 auto;
+  @media (min-width: 768px) and (max-width: 1023px) {
+    margin: 0;
+  }
+`;
+
+export const Title = styled.h3`
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  margin-bottom: 20px;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const Text = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  color: #9b9faa;
+`;
+
+export const RedText = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  color: #fd0000;
+`;
