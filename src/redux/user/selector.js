db21b16f66cde79cbeb0cfd97b395596d6028238
Oseyijotham <jothamoboh16@gmail.com>
2024-10-15 04:43:53 +0100

    Update selector.js

--- src/redux/user/selector.js
+++ src/redux/user/selector.js
@@ -3,5 +3,6 @@ export const getUserDesiredWeight = state => state.user.profileData.dWeight;
 export const getUserAge = state => state.user.profileData.age;
 export const getUserBloodType = state => state.user.profileData.bloodType;
 export const getUserCurrentWeight = state => state.user.profileData.cWeight;
+export const getUserBadFoods = state => state.user.profileData.badFoods;
 export const getUserLoading = state => state.user.isLoading;
 export const getUserError = state => state.user.error;
